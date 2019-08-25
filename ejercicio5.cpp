8b14b9404350943ed03a271fedf1d2c827337f74
Freddy12D <54275776+Freddy12D@users.noreply.github.com>
2019-08-24 22:24:25 -0500

    Update ejercicio5.cpp

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-using std::cout;
+#include <vector>
 
+using std::cout;
+using namespace std;
 int main() {
 
-    vector<int> a {1, 2, 3, 4, 5};
+    std::vector<int> a {1, 2, 3, 4, 5};
     for (auto i : a) {
-        // Imprimir los numeros
+        cout<<i;
     }
 }
