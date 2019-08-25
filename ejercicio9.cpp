1e3349f980af20af3fd66437d254c8d7ef9aa0b0
Freddy12D <54275776+Freddy12D@users.noreply.github.com>
2019-08-24 22:33:35 -0500

    Update ejercicio9.cpp

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -4,9 +4,9 @@ using std::cout;
 int main() {
 
     int i = 1;
-    while (i <= 10) {
-        if (   ) {
-            //imprimir numeros pares verticalmente
+    while (i <= 10) { ++i;
+        if (i%2==0) {
+            cout<<i<<"\n";
         }
 
     }
