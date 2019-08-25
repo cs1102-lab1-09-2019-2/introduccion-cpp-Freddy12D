265c936b85a6e315aff656fcd656dcf366fd4c93
Freddy12D <54275776+Freddy12D@users.noreply.github.com>
2019-08-24 22:31:50 -0500

    Update ejercicio8.cpp

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 using std::cout;
 
-int main() {
+int main() { 
     auto i = 0;
     // Mostrar los numeros del 0 al 4
-    while (i < ) {
-        cout<< i <<"\n";
-
+    while (i < 4) {
+      cout<< ++i <<"\n";
     }
 }
-
